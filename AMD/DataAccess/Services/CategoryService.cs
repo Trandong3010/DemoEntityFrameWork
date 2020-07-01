d7b2d579291f75324419a2298cccc1a002e488ef
trandong-3003 <trandong.x1x0@gmail.com>
2020-07-01 21:40:39 +0700

    add category service

---
+++ AMD/DataAccess/Services/CategoryService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Context;
+using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Services
+{
+    public interface ICategoryService
+    {
+        Task<IList<Categorys>> GetAll();
+
+        Task<Categorys> GetById(int? id);
+
+
+    }
+
+    public class CategoryService : ICategoryService
+    {
+        protected readonly MotelRoomContext _context;
+
+        public async Task<IList<Categorys>> GetAll()
+        {
+            var list = await _context.Categoryses.AsNoTracking().ToListAsync();
+            return list;
+        }
+
+        public Task<Categorys> GetById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
